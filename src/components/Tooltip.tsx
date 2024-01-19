f502dcfb176d1ea356ef62960481bf605abf0e33
[rezaeiar] <rezaeiar0@gmail.com>
2024-01-20 00:03:13 +0330

    fix: fix chart in dhashboard

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,6 +1,6 @@
 const Tooltip = () => {
     return (
-        <div className="absolute top-8 h-10 w-52 shadow-normal rounded-sm bg-white flex items-center justify-center text-general-100 font-nunitosans-regular text-sm">
+        <div className="absolute hidden top-8 h-10 w-52 shadow-normal rounded-sm bg-white flex items-center justify-center text-general-100 font-nunitosans-regular text-sm">
             <div className="w-0 h-0 border-l-transparent border-l-[6px] border-r-transparent border-r-[6px] border-b-white border-b-[6px] absolute -top-1.5"></div>
             See Your Notificatons
         </div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,49 +1,187 @@
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart, Area } from 'recharts';
 
 const Dashboard = () => {
     const data = [
         {
-            name: 'D1',
+            name: '1',
             uv: 4000,
             pv: 2400,
             amt: 2400,
         },
         {
-            name: 'D2',
+            name: '2',
             uv: 3000,
             pv: 1398,
             amt: 2210,
         },
         {
-            name: 'D3',
+            name: '3',
             uv: 2000,
             pv: 9800,
             amt: 2290,
         },
         {
-            name: 'D4',
+            name: '4',
             uv: 2780,
             pv: 3908,
             amt: 2000,
         },
         {
-            name: 'D5',
+            name: '5',
             uv: 1890,
             pv: 4800,
             amt: 2181,
         },
         {
-            name: 'D6',
+            name: '6',
             uv: 2390,
             pv: 3800,
             amt: 2500,
         },
         {
-            name: 'D7',
+            name: '7',
             uv: 3490,
             pv: 4300,
             amt: 2100,
-        }
+        },
+        {
+            name: '8',
+            uv: 7000,
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: '9',
+            uv: 3000,
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: '10',
+            uv: 2000,
+            pv: 9800,
+            amt: 2290,
+        },
+        {
+            name: '1',
+            uv: 4000,
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: '2',
+            uv: 3000,
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: '3',
+            uv: 2000,
+            pv: 9800,
+            amt: 2290,
+        },
+        {
+            name: '4',
+            uv: 2780,
+            pv: 3908,
+            amt: 2000,
+        },
+        {
+            name: '5',
+            uv: 1890,
+            pv: 4800,
+            amt: 2181,
+        },
+        {
+            name: '6',
+            uv: 2390,
+            pv: 3800,
+            amt: 2500,
+        },
+        {
+            name: '7',
+            uv: 3490,
+            pv: 4300,
+            amt: 2100,
+        },
+        {
+            name: '8',
+            uv: 7000,
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: '9',
+            uv: 3000,
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: '10',
+            uv: 2000,
+            pv: 9800,
+            amt: 2290,
+        },
+        {
+            name: '1',
+            uv: 4000,
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: '2',
+            uv: 3000,
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: '3',
+            uv: 2000,
+            pv: 9800,
+            amt: 2290,
+        },
+        {
+            name: '4',
+            uv: 2780,
+            pv: 3908,
+            amt: 2000,
+        },
+        {
+            name: '5',
+            uv: 1890,
+            pv: 4800,
+            amt: 2181,
+        },
+        {
+            name: '6',
+            uv: 2390,
+            pv: 3800,
+            amt: 2500,
+        },
+        {
+            name: '7',
+            uv: 3490,
+            pv: 4300,
+            amt: 2100,
+        },
+        {
+            name: '8',
+            uv: 7000,
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: '9',
+            uv: 3000,
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: '10',
+            uv: 2000,
+            pv: 9800,
+            amt: 2290,
+        },
     ];
 
     return (
@@ -165,28 +303,26 @@ const Dashboard = () => {
                 </div>
                 <div className="h-96 w-full">
                     <ResponsiveContainer width="100%" height="100%">
-                        <BarChart
-                            width={500}
-                            height={300}
-                            data={data}
-                            margin={{
-                                top: 20,
-                                right: 30,
-                                left: 20,
-                                bottom: 5,
-                            }}
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
+                        <AreaChart data={data}>
+                            <defs>
+                                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="#4379EE" stopOpacity={0.8} />
+                                    <stop offset="95%" stopColor="#4379EE" stopOpacity={0} />
+                                </linearGradient>
+                            </defs>
                             <XAxis dataKey="name" />
                             <YAxis />
+                            <CartesianGrid strokeDasharray="3 3" />
                             <Tooltip />
-                            <Legend />
-                            <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-                            <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
-                        </BarChart>
+                            <Area type="monotone" dataKey="pv" stroke="#4379EE" fillOpacity={1} fill="url(#colorUv)" />
+                            <Area type="monotone" dataKey="uv" stroke="#5A607F" fillOpacity={1} fill="url(#colorPv)" />
+                        </AreaChart>
                     </ResponsiveContainer>
                 </div>
             </div>
+            {/* <div className="shadow-md rounded-md h-96 bg-white p-8 flex flex-col gap-y-10">
+
+            </div> */}
         </div>
     )
 }
