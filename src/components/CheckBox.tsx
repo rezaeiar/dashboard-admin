c257f98ca6eb2ba1045c14a29c8c33ad3baa092a
[rezaeiar] <rezaeiar0@gmail.com>
2024-02-13 18:12:11 +0330

    fix: fix SingleCategory and Responsive

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -8,7 +8,7 @@ const CheckBox = ({ setIsChecked, forId }: CheckBoxProps) => {
     return (
         <div className="relative">
             <input onChange={(e) => setIsChecked(e.target.checked)} className="peer hidden" type="checkbox" id={forId} />
-            <label className="duration-300 shadow-lg cursor-pointer w-12 h-6 bg-primary-60 rounded-full flex after:size-5 after:bg-white after:rounded-full items-center after:absolute after:left-0.5 peer-checked:after:left-[26px] rtl:after:right-0.5 rtl:peer-checked:after:right-[26px] after:duration-300 peer-checked:bg-primary-100" htmlFor={forId}></label>
+            <label className="duration-300 shadow-lg cursor-pointer h-4 w-8 xl:w-12 xl:h-6 bg-primary-60 rounded-full flex after:size-3 xl:after:size-5 after:bg-white after:rounded-full items-center after:absolute after:left-0.5 peer-checked:after:left-[18px] xl:peer-checked:after:left-[26px] rtl:after:right-0.5 rtl:peer-checked:after:right-[18px] xl:rtl:peer-checked:after:right-[26px] after:duration-300 peer-checked:bg-primary-100" htmlFor={forId}></label>
         </div>
     )
 }
--- src/pages/SingleCategory.tsx
+++ src/pages/SingleCategory.tsx
@@ -25,21 +25,18 @@ const SingleCategory = () => {
                 <div className="flex gap-x-1 sm:gap-x-2">
                     <Button type="white" size="small" styles="">
                         <>
-                            {t("export")}
+                            {t("cancel")}
                         </>
                     </Button>
                     <Button type="primary" size="small" styles="">
                         <>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                            </svg>
-                            {t("add product")}
+                            {t("save")}
                         </>
                     </Button>
                 </div>
             </div>
-            <div className="grid gap-x-4 grid-cols-7">
-                <div className="bg-white rounded-md p-7 col-span-5 flex flex-col gap-y-6">
+            <div className="flex flex-col md:grid gap-3 lg:gap-4 grid-cols-7">
+                <div className="bg-white rounded-md p-5 xl:p-7 col-span-5 flex flex-col gap-y-4 xl:gap-y-6">
                     <div className="flex gap-x-2 items-center">
                         <h5 className="text-general-100 font-nunitosans-extrabold">
                             Products
@@ -49,17 +46,17 @@ const SingleCategory = () => {
                         </span>
                     </div>
                     <div className="flex flex-col gap-y-3">
-                        <div className="border border-general-50 rounded-md p-4 flex justify-between items-center">
-                            <div className="flex gap-x-4 items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <div className="border border-general-50 rounded-md p-2 sm:p-3 xl:p-4 flex justify-between items-center">
+                            <div className="flex gap-x-3 xl:gap-x-4 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 sm:w-6 sm:h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                                 </svg>
                                 <img src="/images/Bitmap1.png" alt="" className="rounded" />
-                                <h6 className="text-general-100 font-nunitosans-bold">
+                                <h6 className="text-general-100 text-xs sm:text-sm lg:text-base font-nunitosans-bold line-clamp-1">
                                     Women Striped T-Shirt
                                 </h6>
                             </div>
-                            <div className="flex gap-x-4 text-general-70 child:cursor-pointer child:transition-colors">
+                            <div className="flex gap-x-1 sm:gap-x-3 xl:gap-x-4 text-general-70 child:cursor-pointer child:transition-colors">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 hover:text-primary-100">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                 </svg>
@@ -68,17 +65,17 @@ const SingleCategory = () => {
                                 </svg>
                             </div>
                         </div>
-                        <div className="border border-general-50 rounded-md p-4 flex justify-between items-center">
-                            <div className="flex gap-x-4 items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <div className="border border-general-50 rounded-md p-2 sm:p-3 xl:p-4 flex justify-between items-center">
+                            <div className="flex gap-x-3 xl:gap-x-4 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 sm:w-6 sm:h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                                 </svg>
                                 <img src="/images/Bitmap1.png" alt="" className="rounded" />
-                                <h6 className="text-general-100 font-nunitosans-bold">
+                                <h6 className="text-general-100 text-xs sm:text-sm lg:text-base font-nunitosans-bold line-clamp-1">
                                     Women Striped T-Shirt
                                 </h6>
                             </div>
-                            <div className="flex gap-x-4 text-general-70 child:cursor-pointer child:transition-colors">
+                            <div className="flex gap-x-1 sm:gap-x-3 xl:gap-x-4 text-general-70 child:cursor-pointer child:transition-colors">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 hover:text-primary-100">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                 </svg>
@@ -87,17 +84,17 @@ const SingleCategory = () => {
                                 </svg>
                             </div>
                         </div>
-                        <div className="border border-general-50 rounded-md p-4 flex justify-between items-center">
-                            <div className="flex gap-x-4 items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <div className="border border-general-50 rounded-md p-2 sm:p-3 xl:p-4 flex justify-between items-center">
+                            <div className="flex gap-x-3 xl:gap-x-4 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 sm:w-6 sm:h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                                 </svg>
                                 <img src="/images/Bitmap1.png" alt="" className="rounded" />
-                                <h6 className="text-general-100 font-nunitosans-bold">
+                                <h6 className="text-general-100 text-xs sm:text-sm lg:text-base font-nunitosans-bold line-clamp-1">
                                     Women Striped T-Shirt
                                 </h6>
                             </div>
-                            <div className="flex gap-x-4 text-general-70 child:cursor-pointer child:transition-colors">
+                            <div className="flex gap-x-1 sm:gap-x-3 xl:gap-x-4 text-general-70 child:cursor-pointer child:transition-colors">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 hover:text-primary-100">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                 </svg>
@@ -107,46 +104,54 @@ const SingleCategory = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="border border-general-50 rounded-md text-primary-100 flex items-center justify-center p-1.5">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                        </svg>
-                        <span className="font-nunitosans-regular text-sm">
-                            Add Product
-                        </span>
-                    </div>
+                    <Button size="small" type="white" styles="justify-center">
+                        <>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                            <span className="font-nunitosans-regular text-sm">
+                                Add Product
+                            </span>
+                        </>
+                    </Button>
                 </div>
-                <div className="flex flex-col gap-y-4 col-span-2">
-                    <div className="bg-white rounded-md p-7 flex flex-col">
-                        <h5 className="text-general-100 text-sm font-nunitosans-extrabold">
+                <div className="flex flex-col gap-y-3 lg:gap-y-4 col-span-2">
+                    <div className="bg-white rounded-md p-5 xl:p-7 flex flex-col gap-y-4">
+                        <h5 className="text-general-100 text-xs xl:text-sm font-nunitosans-extrabold">
                             Category Visibility
                         </h5>
-                        <CheckBox isChecked={isChecked} setIsChecked={setIsChecked} forId="Visible" />
+                        <div className="flex items-center gap-x-2">
+                            <CheckBox isChecked={isChecked} setIsChecked={setIsChecked} forId="Visible" />
+                            <span className="text-xs font-nunitosans-regular text-general-90">
+                                Visible on site
+                            </span>
+                        </div>
                     </div>
-                    <div className="bg-white rounded-md p-7 flex flex-col gap-y-4">
-                        <h5 className="text-general-100 text-sm font-nunitosans-extrabold">
-                            Category Visibility
+                    <div className="bg-white rounded-md p-5 xl:p-7 flex flex-col gap-y-4">
+                        <h5 className="text-general-100 text-xs xl:text-sm font-nunitosans-extrabold">
+                            Category Info
                         </h5>
                         <div className="flex flex-col">
-                            <label htmlFor="" className="text-sm text-general-60 font-nunitosans-regular">
+                            <label htmlFor="" className="text-xs lg:text-sm text-general-60 font-nunitosans-regular">
                                 Name
                             </label>
-                            <input type="text" className="border border-general-50 outline-none rounded text-sm text-general-100 py-2 px-4" value={'Women Clothes'} />
+                            <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4" value={'Women Clothes'} />
                         </div>
                         <div className="flex flex-col">
-                            <label htmlFor="" className="text-sm text-general-60 font-nunitosans-regular">
+                            <label htmlFor="" className="text-xs lg:text-sm text-general-60 font-nunitosans-regular">
                                 Image
                             </label>
-                            <div className="border border-dashed border-general-60 rounded-md flex items-center justify-center py-12">
+                            <label className="border cursor-pointer border-dashed border-general-60 rounded-md flex items-center justify-center aspect-[4/3]" htmlFor="small_size">
                                 <div className="flex items-center justify-center flex-col gap-y-2">
-                                    <button className="flex border border-general-50 text-xs text-primary-100 bg-white px-5 py-2 rounded gap-x-2 items-center hover:bg-general-40 transition-colors hover:border-general-50">
+                                    <div className="py-2 sm:py-1.5 xl:py-2 px-5 sm:px-2 xl:px-5 text-xs xl:text-sm capitalize bg-white text-primary-100 rounded font-nunitosans-regular rtl:font-iransans-regular h-min hover:bg-general-30 active:bg-white disabled:bg-general-50 disabled:text-white focus:bg-white transition-colors border border-general-50 flex items-center gap-x-1">
                                         Add File
-                                    </button>
-                                    <span className="text-general-80 text-sm font-nunitosans-regular">
+                                    </div>
+                                    <span className="text-general-80 text-sm sm:text-[10px] xl:text-sm font-nunitosans-regular">
                                         Or drag and drop files
                                     </span>
                                 </div>
-                            </div>
+                            </label>
+                            <input className="w-full mb-5 text-xs text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 hidden" id="small_size" type="file" />
                         </div>
                     </div>
                 </div>
