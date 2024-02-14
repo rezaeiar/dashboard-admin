c09f7138ba18acd44ef7ba182688dccc53f24b27
[rezaeiar] <rezaeiar0@gmail.com>
2024-02-14 16:08:39 +0330

    fix: Bilingual AddProduct

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -1,8 +1,11 @@
 import Button from "../components/Button"
 import { useTranslation } from "react-i18next"
+import CheckBox from "../components/CheckBox"
+import { useState } from "react"
 
 const AddProduct = () => {
     const { t } = useTranslation()
+    const [isChecked, setIsChecked] = useState(false)
     return (
         <div className="py-4 sm:py-6 md:py-8 px-4 sm:px-6 md:px-8 w-full bg-general-30 flex flex-col gap-y-4 sm:gap-y-6 md:gap-y-8 overflow-hidden">
             <div className="flex justify-between items-center">
@@ -35,101 +38,121 @@ const AddProduct = () => {
             <div className="flex flex-col md:grid gap-3 lg:gap-4 grid-cols-7">
                 <div className="bg-white rounded-md p-5 xl:p-7 col-span-5 flex flex-col gap-y-6 xl:gap-y-8 divide-y">
                     <div className="flex flex-col gap-y-6">
-                        <h5 className="text-general-100 text-sm xl:text-base font-nunitosans-extrabold">
-                            Information
+                        <h5 className="text-general-100 text-sm xl:text-base ltr:font-nunitosans-extrabold rtl:font-iransans-bold">
+                            {t("information")}
                         </h5>
                         <div className="flex flex-col">
-                            <label htmlFor="" className="text-xs lg:text-sm text-general-60 font-nunitosans-regular">
-                                Title
+                            <label htmlFor="" className="text-xs lg:text-sm text-general-60 ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                                {t("product name")}
                             </label>
-                            <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4" placeholder="Enter tag name" />
+                            <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4 ltr:font-nunitosans-regular rtl:font-iransans-regular" placeholder={t("Summer T-Shirt")} />
                         </div>
                         <div className="flex flex-col">
-                            <label htmlFor="" className="text-xs lg:text-sm text-general-60 font-nunitosans-regular">
-                                Title
+                            <label htmlFor="" className="text-xs lg:text-sm text-general-60 ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                                {t("product description")}
                             </label>
-                            <textarea name="" id="" className="border border-general-50 outline-none rounded text-sm text-general-100 aspect-[5/1] px-4"></textarea>
+                            <textarea name="" id="" className="border py-2 border-general-50 outline-none rounded text-sm text-general-100 aspect-[5/1] px-4 ltr:font-nunitosans-regular rtl:font-iransans-regular"></textarea>
                         </div>
                     </div>
                     <div className="flex flex-col gap-y-6 pt-4 xl:pt-6">
-                        <h5 className="text-general-100 text-sm xl:text-base font-nunitosans-extrabold">
-                            Images
+                        <h5 className="text-general-100 text-sm xl:text-base ltr:font-nunitosans-extrabold rtl:font-iransans-bold">
+                            {t("images")}
                         </h5>
-                        <label className="border cursor-pointer border-dashed border-general-60 rounded-md flex items-center justify-center aspect-square sm:aspect-[6/2]" htmlFor="small_size">
+                        <label className="border cursor-pointer border-dashed bg-general-30/40 hover:bg-general-30/80 transition-all border-general-60 rounded-md flex items-center justify-center aspect-square sm:aspect-[6/2]" htmlFor="small_size">
                             <div className="flex items-center justify-center flex-col gap-y-2">
-                                <div className="py-2 sm:py-1.5 xl:py-2 px-5 sm:px-2 xl:px-5 text-xs xl:text-sm capitalize bg-white text-primary-100 rounded font-nunitosans-regular rtl:font-iransans-regular h-min hover:bg-general-30 active:bg-white disabled:bg-general-50 disabled:text-white focus:bg-white transition-colors border border-general-50 flex items-center gap-x-1">
-                                    Add File
-                                </div>
-                                <span className="text-general-80 text-sm sm:text-[10px] xl:text-sm font-nunitosans-regular">
-                                    Or drag and drop files
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 text-general-60">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                                </svg>
+                                <span className="text-general-80 text-sm sm:text-[10px] xl:text-sm ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                                    {t("or drag and drop files")}
                                 </span>
                             </div>
                         </label>
                         <input className="w-full mb-5 text-xs text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 hidden" id="small_size" type="file" />
                     </div>
                     <div className="flex flex-col gap-y-6 pt-4 xl:pt-6">
-                        <h5 className="text-general-100 text-sm xl:text-base font-nunitosans-extrabold">
-                            Price
+                        <h5 className="text-general-100 text-sm xl:text-base ltr:font-nunitosans-extrabold rtl:font-iransans-bold">
+                            {t("price")}
                         </h5>
                         <div className="grid grid-cols-2 gap-x-4">
                             <div className="flex flex-col">
-                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 font-nunitosans-regular">
-                                    Product Price
+                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                                    {t("product price")}
                                 </label>
-                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4" placeholder="Enter price" />
+                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4 ltr:font-nunitosans-regular rtl:font-iransans-regular" placeholder={t("enter price")} />
                             </div>
                             <div className="flex flex-col">
-                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 font-nunitosans-regular">
-                                    Discount Price
+                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                                    {t("discount price")}
                                 </label>
-                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4" placeholder="Price at Discount" />
+                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4 ltr:font-nunitosans-regular rtl:font-iransans-regular" placeholder={t("price at discount")} />
                             </div>
+                        </div>
+                        <div className="flex items-center gap-x-2">
+                            <CheckBox isChecked={isChecked} setIsChecked={setIsChecked} forId="Visible" />
+                            <span className="text-xs ltr:font-nunitosans-regular rtl:font-iransans-regular text-general-90">
+                                {t("Add tax for this product")}
+                            </span>
                         </div>
                     </div>
                     <div className="flex flex-col gap-y-6 pt-4 xl:pt-6">
-                        <h5 className="text-general-100 text-sm xl:text-base font-nunitosans-extrabold">
-                            Different Options
+                        <h5 className="text-general-100 text-sm xl:text-base ltr:font-nunitosans-extrabold rtl:font-iransans-bold">
+                            {t("Different Options")}
                         </h5>
+                        <div className="flex items-center gap-x-2">
+                            <CheckBox isChecked={isChecked} setIsChecked={setIsChecked} forId="Visible" />
+                            <span className="text-xs ltr:font-nunitosans-regular rtl:font-iransans-regular text-general-90">
+                                {t("This product has multiple options")}
+                            </span>
+                        </div>
                         <div className="grid grid-cols-2 gap-x-4">
                             <div className="flex flex-col">
-                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 font-nunitosans-regular">
-                                    Size
+                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                                    {t("title")}
                                 </label>
-                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4" placeholder="Sizet" />
+                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4 ltr:font-nunitosans-regular rtl:font-iransans-regular" placeholder={t("enter title")} />
                             </div>
                             <div className="flex flex-col">
-                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 font-nunitosans-regular">
-                                    Value
+                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                                    {t("value")}
                                 </label>
-                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4" placeholder="Price at Discount" />
+                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4 ltr:font-nunitosans-regular rtl:font-iransans-regular" placeholder={t("enter value")} />
                             </div>
                         </div>
-                        <span className="text-primary-100 text-sm font-nunitosans-regular">Create New</span>
+                        <span className="text-primary-100 text-sm ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                            {t("Create New")}
+                        </span>
                     </div>
                     <div className="flex flex-col gap-y-6 pt-4 xl:pt-6">
-                        <h5 className="text-general-100 text-sm xl:text-base font-nunitosans-extrabold">
-                            Shipping
+                        <h5 className="text-general-100 text-sm xl:text-base ltr:font-nunitosans-extrabold rtl:font-iransans-bold">
+                            {t("shipping")}
                         </h5>
                         <div className="grid grid-cols-2 gap-x-4">
                             <div className="flex flex-col">
-                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 font-nunitosans-regular">
-                                    Weight
+                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                                    {t("Weight")}
                                 </label>
-                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4" placeholder="Enter Weight" />
+                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4 ltr:font-nunitosans-regular rtl:font-iransans-regular" placeholder={t("Enter Weight")} />
                             </div>
                             <div className="flex flex-col">
-                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 font-nunitosans-regular">
-                                    Country
+                                <label htmlFor="" className="text-xs lg:text-sm text-general-60 ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                                    {t("Country")}
                                 </label>
-                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4" placeholder="Select Country" />
+                                <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4 ltr:font-nunitosans-regular rtl:font-iransans-regular" placeholder={t("Select Country")} />
                             </div>
+                        </div>
+                        <div className="flex items-center gap-x-2">
+                            <CheckBox isChecked={isChecked} setIsChecked={setIsChecked} forId="Visible" />
+                            <span className="text-xs ltr:font-nunitosans-regular rtl:font-iransans-regular text-general-90">
+                                {t("This is digital item")}
+                            </span>
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-col gap-y-3 lg:gap-y-4 col-span-2">
                     <div className="bg-white rounded-md p-5 xl:p-7 flex flex-col gap-y-4">
-                        <h5 className="text-general-100 text-xs xl:text-sm font-nunitosans-extrabold">
-                            Categories
+                        <h5 className="text-general-100 text-xs xl:text-sm ltr:font-nunitosans-extrabold rtl:font-iransans-bold">
+                            {t("categories")}
                         </h5>
                         <ul>
                             <li className="text-general-100 text-xs lg:text-sm flex items-center gap-x-2">
@@ -141,19 +164,19 @@ const AddProduct = () => {
                                 <span>Men</span>
                             </li>
                         </ul>
-                        <span className="text-primary-100 text-xs lg:text-sm font-nunitosans-regular">
-                            Create New
+                        <span className="text-primary-100 text-xs lg:text-sm ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                            {t("Create New")}
                         </span>
                     </div>
                     <div className="bg-white rounded-md p-5 xl:p-7 flex flex-col gap-y-4">
-                        <h5 className="text-general-100 text-xs xl:text-sm font-nunitosans-extrabold">
-                            Tags
+                        <h5 className="text-general-100 text-xs xl:text-sm ltr:font-nunitosans-extrabold rtl:font-iransans-bold">
+                            {t("Tags")}
                         </h5>
                         <div className="flex flex-col">
-                            <label htmlFor="" className="text-xs lg:text-sm text-general-60 font-nunitosans-regular">
-                                Add Tags
+                            <label htmlFor="" className="text-xs lg:text-sm text-general-60 ltr:font-nunitosans-regular rtl:font-iransans-regular">
+                                {t("Add Tags")}
                             </label>
-                            <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4" placeholder="Enter tag name" />
+                            <input type="text" className="border border-general-50 outline-none rounded text-xs lg:text-sm text-general-100 py-2 md:py-1.5 lg:py-2 px-4 md:px-2.5 lg:px-4 ltr:font-nunitosans-regular rtl:font-iransans-regular" placeholder={t("Enter tag name")} />
                         </div>
                         <div className="flex flex-wrap gap-2">
                             <div className="flex bg-general-40 rounded text-general-80 items-center gap-x-2 text-[10px] lg:text-xs py-1 px-2">
